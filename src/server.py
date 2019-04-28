0ecbaaf311a24320b1a1e87415d0fde3d7f5cee4
Michel <13646125+michelBBC@users.noreply.github.com>
2019-04-28 15:17:38 +0100

    Merged correct and search enpoints into one. Refactoring.

--- src/server.py
+++ src/server.py
@@ -1,21 +1,36 @@
 from flask import Flask, request
+from flask_selfdoc import Autodoc
 from flask.wrappers import Response
 
 import json
+import os
 
 from .utils import *
 
 json_headers = {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'}
 
 app = Flask(__name__)
+auto = Autodoc(app)
 
-@app.route("/", methods=["GET"])
+api_version = os.getenv('api_version')
+
+@app.route('/', methods=['GET'])
+def help():
+    """ Display help. """
+    # return auto.html()
+    return auto.html(title='Search with spellchecker API: endpoints', template="autodoc_custom.html")
+
+@app.route("/ping", methods=["GET"])
+@auto.doc()
 def hello():
-    """ Ping (alive test) endpoint. """
+    """
+    Ping (alive test).
+    """
     return Response("Hello", 200)
 
 
 @app.route("/search", methods=["GET", "POST"])
+@auto.doc()
 def search_items():
     """
     Search for products by name.
@@ -25,43 +40,35 @@ def search_items():
         payload = request.get_json(silent=True)
         if not payload:
             # Bad payload, return with user error
-            return Response("Missing or ill-formatted query. For POST requests, payload of format {'query': '<word>'} is required as well as correct headers ('Content-Type': 'application/json').", 400)
-        query = payload.get("query")
-        mixin = payload.get("mixin")
-        max_hits = payload.get("max")
+            return Response("Missing or ill-formatted query. For POST requests, payload of format {'query': '<word>'} and correct headers ('Content-Type': 'application/json') are required.", 400)
     else:
         # Get request
-        query = request.args.get('q') or request.args.get('query')
-        mixin = request.args.get("mixin")
-        max_hits = request.args.get("max")
+        payload = request.args
+
+    # Get parameters of request
+    query = payload.get("query") or payload.get("q")
+    mixin = payload.get("mixin")
+    max_hits = payload.get("max")
+    print(bool(payload.get("suggestions")))
+    spell_check = payload.get("suggestions") == 'true' if "suggestions" in payload else False
     if not query: 
         return Response("Empty query", 204)
-    spell_check = False
+
+
+    # Do spelling check
     if spell_check:
         query = " ".join(w for w in check_spelling(query))
+        corrected = [w for w in check_spelling(query)]
+
+    # Get search results
     max_h = max_hits or 10
     search_response = search_term(query, max_hits=max_h, mixin=mixin)
-    return Response(json.dumps(search_response), 200, headers=json_headers)
 
-
-@app.route("/correct", methods=["GET", "POST"])
-def correct_items():
-    if request.method == "POST":
-        # Post request
-        payload = request.get_json(silent=True)
-        if not payload:
-            # Bad payload, return with user error
-            return Response("Missing or ill-formatted query. For POST requests, payload of format {'query': 'word'} is expected as well as correct headers for Content-Type.", 400)
-        query = payload.get("query")
+    # Format response
+    if spell_check:
+        response = {'results': search_response, 'spelling_suggestions': corrected}
     else:
-        # Get request
-        query = request.args.get('q') or request.args.get('query')
-    if not query: 
-        return Response("Empty input", 204)
-    corrected = [w for w in check_spelling(query)]
-    return Response(json.dumps(corrected), 200, headers=json_headers)
+        response = {'results': search_response}
 
-
-if __name__ == "__main__":
-    app.running_update = True
-    http_server = app.run(host="0.0.0.0", port=8080, debug=True)
+    response['metadata'] = {'request': request.url, 'version': api_version}
+    return Response(json.dumps(response), 200, headers=json_headers)
